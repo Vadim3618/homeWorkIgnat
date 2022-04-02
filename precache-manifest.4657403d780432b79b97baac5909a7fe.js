6bc569129b9fa44ff59e6309130b94b8573668b3
vdmdzr <vdmdzr@gmail.com>
2022-04-02 18:51:22 +0300

    Updates

---
+++ precache-manifest.4657403d780432b79b97baac5909a7fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8b525a4debdda90e13379ecea640d44",
+    "url": "/homeWorkIgnat/index.html"
+  },
+  {
+    "revision": "0de83605fdf8a9486d40",
+    "url": "/homeWorkIgnat/static/css/main.301daf5a.chunk.css"
+  },
+  {
+    "revision": "a2bf31979b8eb927a7da",
+    "url": "/homeWorkIgnat/static/js/2.5ec98555.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homeWorkIgnat/static/js/2.5ec98555.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0de83605fdf8a9486d40",
+    "url": "/homeWorkIgnat/static/js/main.30665ff2.chunk.js"
+  },
+  {
+    "revision": "1651273c2e1a51fcafce",
+    "url": "/homeWorkIgnat/static/js/runtime-main.7d546f4f.js"
+  }
+]);
